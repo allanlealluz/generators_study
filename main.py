9f7b8ec6cafba63540c140007cb454e491f7dbab
allanlealluz <allanlealluz@gmail.com>
2022-01-26 16:03:37 -0300

    yield from

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ def impares():
     while True:
         yield valor
         valor += 2
-
+def sem_from():
+    for valores in [1,2,3,5,4,65]:
+        yield valores
+def com_from():
+    yield from [1,2,3,5,4,65]
